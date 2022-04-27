c7badb3570c4dd195414a66fdeffe8369301c25d
Ahmed Warsama <ahmedwarsama@Ahmeds-MacBook-Air.local>
2022-04-27 17:35:19 +0100

    updated firebase config file and added Home css file and begun setting data in home.js file

--- src/firebase.js
+++ src/firebase.js
@@ -2,15 +2,25 @@ import firebase from 'firebase/compat/app';
 import { initializeApp } from "firebase/app";
 import 'firebase/compat/database'
 
-const firebaseConfig = {
-    apiKey: "AIzaSyB_GjmYED5xqGdqPm8gvkaSg3ntFJdnTPw",
-    authDomain: "react-contactsss.firebaseapp.com",
-    projectId: "react-contactsss",
-    storageBucket: "react-contactsss.appspot.com",
-    messagingSenderId: "466214171728",
-    appId: "1:466214171728:web:f14ec54ffbd788d0d38d55",
-    measurementId: "G-Q2QF2M4QH6"
-  };
+// const firebaseConfig = {
+//     apiKey: "AIzaSyB_GjmYED5xqGdqPm8gvkaSg3ntFJdnTPw",
+//     authDomain: "react-contactsss.firebaseapp.com",
+//     projectId: "react-contactsss",
+//     storageBucket: "react-contactsss.appspot.com",
+//     messagingSenderId: "466214171728",
+//     appId: "1:466214171728:web:f14ec54ffbd788d0d38d55",
+//     measurementId: "G-Q2QF2M4QH6"
+//   };
   
+  const firebaseConfig = {
+  apiKey: "AIzaSyD-sIcdGoX4y8UuU8z4L6g524lESVHpI3c",
+  authDomain: "test-ivy-tools.firebaseapp.com",
+  databaseURL: "https://test-ivy-tools-default-rtdb.firebaseio.com",
+  projectId: "test-ivy-tools",
+  storageBucket: "test-ivy-tools.appspot.com",
+  messagingSenderId: "995311459518",
+  appId: "1:995311459518:web:17f41dc217a1783d30d2ef",
+  measurementId: "G-M6GFDTPYKS"
+};
   const fireDb = firebase.initializeApp(firebaseConfig)
   export default fireDb.database().ref();
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,20 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import fireDb from "../firebase"
+import {Link} from "react-router-dom"
+import './Home.css'
 
 export default function Home() {
+    const[data, setData] = useState({})
+    useEffect(()=> {
+    // DB name is contact
+        fireDb.child("contact").on("value", (snapshot) => {
+            if(snapshot.val()!== null) {
+                setData({...snapshot.val()})
+            } else{
+                setData({})
+            }
+        })
+    })
     return(
 <div>
 <h2> Home</h2>
