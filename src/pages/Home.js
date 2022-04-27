99fb21068b1fb152687b54a24fc7db696fc90f5a
Ahmed Warsama <ahmedwarsama@Ahmeds-MacBook-Air.local>
2022-04-27 17:57:55 +0100

    listed out basic database data to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,60 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import fireDb from "../firebase"
-import {Link} from "react-router-dom"
+// import { Link } from "react-router-dom"
 import './Home.css'
 
 export default function Home() {
     const[data, setData] = useState({})
+
     useEffect(()=> {
-    // DB name is contact
-        fireDb.child("contact").on("value", (snapshot) => {
-            if(snapshot.val()!== null) {
-                setData({...snapshot.val()})
+        // DB name is contact
+        fireDb.child("contacts").on("value", (snapshot) => {
+            if(snapshot.val() !== null) {
+                setData({...snapshot.val() })
             } else{
                 setData({})
             }
         })
-    })
+
+        return () => {
+            setData ({})
+        }
+    }, [])
     return(
-<div>
-<h2> Home</h2>
+<div style={{marginTop: "100px"}}>
+    <table className="styled-table">
+        <thead>
+            <tr>
+                <th style={{textAlign: "center"}}>
+                    No.
+                </th>
+                <th style={{textAlign: "center"}}>
+                    Name
+                </th>
+                <th style={{textAlign: "center"}}>
+                    email
+                </th>
+                  <th style={{textAlign: "center"}}>
+                    Contact
+                </th>
+                  <th style={{textAlign: "center"}}>
+                    Action
+                </th>
+            </tr >
+        </thead>
+        <tbody>
+            {Object.keys(data).map((id, index)=> {
+                return (
+                    <tr key={id}>
+                        <th scope="row"> {index+1} </th>
+                            <td>  {data[id].name} </td>
+                            <td>  {data[id].email} </td>
+                            <td>  {data[id].contact} </td>
+                    </tr>
+                )
+            })}
+        </tbody>
+    </table>
 </div>
     )
 }
