9b045101bdb305e2eb1e6247fdbfdd652aaae928
Ahmed Warsama <ahmedwarsama@Ahmeds-MacBook-Air.local>
2022-04-27 16:39:42 +0100

    adding add contact form css

--- src/pages/AddEdit.js
+++ src/pages/AddEdit.js
@@ -10,15 +10,50 @@ email: "",
 contact: ""
 }
 export default function AddEdit() {
-const [state, setState] = useState(initialState);
-const[data, setData] = useState({});
-const {name, email, contact} = state;
+
+    const [state, setState] = useState(initialState);
+    const[data, setData] = useState({});
+    const {name, email, contact} = state;
+
+    const handleSubmit = () => {};
+    const handleInputChange = () => {};
+
     return(
-<div style={{marginTop: "100px"}}>
-    <form style={{margin: "auto", padding: "15px", maxWidth: "400px", alignContent: "center"}}>
+        <div style={{marginTop: "100px"}}>
+            <form 
+            style={{margin: "auto", padding: "15px", maxWidth: "400px", alignContent: "center"}}
+            onSubmit={handleSubmit}
+            >
+                <label htmlFor="name">name</label>
+                <input 
+                    type="text"
+                    id="name"
+                    name="name"
+                    placeHolder="your name..."
+                    value = {name}
+                    onChange={handleInputChange}
+                ></input>
+                <label htmlFor="email">Email</label>
+                <input 
+                    type="email"
+                    id="email"
+                    name="email"
+                    placeHolder="your email..."
+                    value = {email}
+                    onChange={handleInputChange}
+                ></input>
+                <label htmlFor="contact">Contact</label>
+                <input 
+                    type="number"
+                    id="contact"
+                    name="contact"
+                    placeHolder="your contact..."
+                    value = {contact}
+                    onChange={handleInputChange}
+                ></input>
 
-
-    </form>
-</div>
+                <input type="submit" value="save" ></input>
+            </form>
+        </div>
     )
 }
