6da415fd33ac5c57a20636a1d94a67ba4f145296
Ahmed Warsama <ahmedwarsama@Ahmeds-MacBook-Air.local>
2022-04-28 10:48:16 +0100

    view sorted

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,7 +3,9 @@ import React from "react";
 export default function About() {
     return(
 <div>
-<h2> About</h2>
+<h2> 
+    This is a react contact system with routing using firebase.
+</h2>
 </div>
     )
 }
--- src/pages/View.js
+++ src/pages/View.js
@@ -1,9 +1,54 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import fireDb from "../firebase";
+import {useParams, Link } from 'react-router-dom';
+import './View.css'
 
 export default function View() {
+
+    const [user, setUser] = useState({})
+    // grabbig the id using the useparam hook we importaed above
+    const {id} = useParams()
+
+    useEffect(()=>{
+// getting the single contact from database
+        fireDb.child(`contacts/${id}`).get().then((snapshot) => {
+            if (snapshot.exists()) {
+                setUser({...snapshot.val()})
+            } else {
+                setUser({})
+            }
+        })
+    },[id])
+    console.log("user", user)
     return(
-        <div>
-        <h2> View </h2>
+        <div style={{marginTop: "150px"}}>
+            <div className="card">
+                        <div className="container">
+                            <div className="card-header">
+                        <p>User Contact Detail</p> 
+                        </div>
+                            <strong>ID: </strong>
+                            <span>{id}</span>
+                            <br></br>
+                            <br></br>
+                            <strong>Name: </strong>
+                            <span>{user.name}</span>
+                            <br></br>
+                            <br></br>
+                            <strong>Email: </strong>
+                            <span>{user.email}</span>
+                            <br></br><br></br>
+                            <strong>Contact: </strong>
+                            <span>{user.contact}</span>
+                            <br></br>
+                            <br></br>
+                            <Link to="/">
+                                <button className="btn btn-edit">
+                                     Go back
+                                </button>
+                            </Link>
+                    </div>
+            </div>
         </div>
     )
 }
